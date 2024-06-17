532771ba985a3f11f72959f1a976c478c2196e21
ginceTechfriar <gince@techfriar.com>
2024-06-17 10:13:36 +0530

    data added to db

---
+++ prisma/migrations/20240617033413_create_users_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "profile_image_url" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,22 +1,59 @@
 import { ApolloServer } from "@apollo/server";
 import { gql } from "graphql-tag";
 import { User } from "./user/index";
+import { prismaClient } from "../lib/db";
 
 async function createGraphqlServer() {
   //create graphql server
   const gqlServer = new ApolloServer({
     typeDefs: gql`
       type Query {
-       hello:String
+        hello: String
       }
 
       type Mutation {
-        ${User.mutations}
+        createUser(
+          firstName: String!
+          lastName: String!
+          email: String!
+          password: String!
+        ): Boolean
       }
     `,
     resolvers: {
       Query: { ...User.resolvers.queries },
-      Mutation: { ...User.resolvers.mutations },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          try {
+            const result = await prismaClient.user.create({
+              data: {
+                firstName,
+                lastName,
+                email,
+                password,
+                salt: "testtt",
+              },
+            });
+            console.log(result);
+          } catch (error) {
+            console.log(error);
+          }
+          return true;
+        },
+      },
     },
   });
 
